4729dbdfb44aff40aa6f7f0b60a126e1b9f41c84
marsopolo <105975781+marsopolo@users.noreply.github.com>
2022-06-02 18:41:53 +0800

    edir

--- observer.js
+++ observer.js
@@ -25,6 +25,85 @@ faders.forEach(fader => {
 
 
 
+// const observerOptions = {
+//     threshold: 0.5,
+//     rootMargin: "0px 0px 0px 0px"
+// }
+
+// const observer = new IntersectionObserver(entries => {
+//     entries.forEach(entry => {
+//         const square = entry.target.querySelector('.title__wrapper');
+
+//         if (!entry.isIntersecting) {
+//             square.classList.add('slide-down');
+//             return;
+//         } else {
+//             square.classList.remove('slide-down');
+//         }
+//     });
+// }, observerOptions);
+
+// observer.observe(document.querySelector('.title__wrapper'));
+
+
+
+// const observer = new IntersectionObserver(entries => {
+//     entries.forEach(entry => {
+//         if (entry.isIntersecting) {
+//             entry.target.classList.add('slide-down');
+//         }
+//     });
+// });
+
+// observer.observe(document.querySelector('.slidedown'));
+
+
+
+
+// function reveal() {
+//     var reveals = document.querySelectorAll(".reveal");
+  
+//     for (var i = 0; i < reveals.length; i++) {
+//       var window = window.innerWidth;
+//     //   var elementVisible = reveals[i].getBoundingClientRect().top;
+//       var elementVisible = 150;
+  
+//       if (elementVisible) {
+//         reveals[i].classList.add("active");
+//       } else {
+//         reveals[i].classList.remove("active");
+//       }
+//     }
+//   }
+  
+// window.addEventListener("scroll", reveal);
+
+
+
+
+
+
+// ORIGINAL CODE
+
+// function reveal() {
+//     var reveals = document.querySelectorAll(".reveal");
+  
+//     for (var i = 0; i < reveals.length; i++) {
+//       var windowHeight = window.innerHeight;
+//       var elementTop = reveals[i].getBoundingClientRect().top;
+//       var elementVisible = 150;
+  
+//       if (elementTop < windowHeight - elementVisible) {
+//         reveals[i].classList.add("active");
+//       } else {
+//         reveals[i].classList.remove("active");
+//       }
+//     }
+//   }
+  
+//   window.addEventListener("scroll", reveal);
+
+
 // var initialSrc = "/images/sprint_stand.png";
 // var scrollSrc = "/images/sprint_walk.png";
 
